bb2e65ce407b38b64c99a50be5e0eb5a8d2ec0a5
Hop11 <97374925+Hop11@users.noreply.github.com>
2022-01-14 23:08:41 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Thế mình làm ngiu nhau nha🥰.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://hopnguyenvan11' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/vanhopnguyen11' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
